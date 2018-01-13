b6d939d718c49d5b8224951d6992d68e511587fb
tjb1272 <30851416+tjb1272@users.noreply.github.com>
2018-01-13 15:35:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const handlebars = require('express-handlebars').create({ defaultLayout: 'main' })
 const db = new sqlite3.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");
+// const stmt = db.prepare(`INSERT INTO Artist (ArtistId, Name) VALUES (?, ?)`);
 
 app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
@@ -26,22 +27,29 @@ db.each(`SELECT * FROM artist LEFT OUTER JOIN album using (ArtistId)`, (err, row
 
 db.close();
 
-app.get('/', (req, res) => {
+app.get('/album', (req, res) => {
+  console.log('ArtistId: ' + res.body.artistid);
+  console.log('Title: ' + res.body.title);
   res.render('home')
 })
 
-
-app.post('/album', (req, res) => {
+app.get('/artist', (req, res) => {
   console.log('ArtistId: ' + res.body.artistid);
-  console.log('Title: ' + res.body.title);
-  res.send('home')
+  console.log('Title: ' + res.body.name);
+  res.render('home')
 })
 
-app.post('/artist', (req, res) => {
-  console.log('ArtistId: ' + res.body.artistid);
-  console.log('Name: ' + res.body.name);
-  res.send('home')
-})
+// app.post('/album', (req, res) => {
+//   console.log('ArtistId: ' + req.body.artistid);
+//   console.log('Title: ' + req.body.title);
+//   res.send('home')
+// })
+
+// app.post('/artist', (req, res) => {
+//   console.log('ArtistId: ' + req.body.artistid);
+//   console.log('Name: ' + req.body.name);
+//   res.send('home')
+// })
 
 app.use((req, res) => {
   res.status(400);
