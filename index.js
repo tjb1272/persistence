e3e9819b2e22ea4d1ab63f23a8db6c4ea0f05050
tjb1272 <30851416+tjb1272@users.noreply.github.com>
2018-01-14 16:44:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser')
 const app = express()
 const handlebars = require('express-handlebars').create({ defaultLayout: 'main' })
 const db = new sqlite3.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");
-// const stmt = db.prepare(`INSERT INTO Artist (ArtistId, Name) VALUES (?, ?)`);
 
 app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
@@ -19,14 +18,6 @@ var req = http.request(options, function(res) {
 );
 req.end();
 
-
-// db.each(`SELECT * FROM artist LEFT OUTER JOIN album using (ArtistId)`, (err, row) => {
-//   if (err) throw err;
-//   console.log(row);
-// });
-
-
-
 app.get('/album', (req, res) => {
   db.all(`SELECT * FROM artist LEFT OUTER JOIN album using (ArtistId)`, (err, row) => {
     if (err) throw err;
@@ -36,13 +27,6 @@ app.get('/album', (req, res) => {
   });
   db.close();
 })
-
-app.get('/artist', (req, res) => {
-  // console.log('ArtistId: ' + req.body.artistid);
-  console.log('Name: ' + req.body.name);
-  res.render('home')
-})
-
 
 app.use((req, res) => {
   res.status(400);
